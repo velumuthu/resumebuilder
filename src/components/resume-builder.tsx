4babd1846478906f26d7a1c9651fed987e52d56a
VELU M <velumbalaji@gmail.com>
2025-08-28 18:13:54 +0000

    in desktop align in printing not perfect

--- src/components/resume-builder.tsx
+++ src/components/resume-builder.tsx
@@ -269,8 +269,10 @@ export default function ResumeBuilder() {
           </Tabs>
         </div>
         {/* Print View */}
-        <div className="hidden print:block">
+        <div className="hidden print:block print-container">
+          <div className="mx-auto w-[8.5in] h-[11in]">
             <ResumePreview resumeData={data} />
+          </div>
         </div>
       </main>
     </div>
