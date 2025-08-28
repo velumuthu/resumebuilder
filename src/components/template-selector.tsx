19cfc06257724b398945f8420565372bdfd08894
VELU M <velumbalaji@gmail.com>
2025-08-28 17:57:01 +0000

    https://i.imghippo.com/files/XuF5864Yvg.png this is for elegant

--- src/components/template-selector.tsx
+++ src/components/template-selector.tsx
@@ -18,7 +18,7 @@ const templates = [
   { id: 'minimalist', name: 'Minimalist', imageUrl: 'https://i.imghippo.com/files/OpV7754i.png', aiHint: 'minimalist resume' },
   { id: 'academic', name: 'Academic', imageUrl: 'https://i.imghippo.com/files/jrJS7434DBo.png', aiHint: 'academic cv' },
   { id: 'corporate', name: 'Corporate', imageUrl: 'https://i.imghippo.com/files/mM6306YPQ.png', aiHint: 'corporate resume' },
-  { id: 'elegant', name: 'Elegant', imageUrl: 'https://i.imghippo.com/files/S0gWf1722429953.png', aiHint: 'elegant resume' },
+  { id: 'elegant', name: 'Elegant', imageUrl: 'https://i.imghippo.com/files/XuF5864Yvg.png', aiHint: 'elegant resume' },
   { id: 'bold', name: 'Bold', imageUrl: 'https://i.imghippo.com/files/MwX4896mA.png', aiHint: 'bold resume' },
   { id: 'modern', name: 'Modern', imageUrl: 'https://i.imghippo.com/files/XuF5864Yvg.png', aiHint: 'modern resume' },
 ] as const;
