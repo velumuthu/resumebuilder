5b574a014dd7f842a4fa8ff26378939d03f86323
VELU M <velumbalaji@gmail.com>
2025-08-28 17:54:06 +0000

    https://i.imghippo.com/files/MwX4896mA.png this is for bold

--- src/components/template-selector.tsx
+++ src/components/template-selector.tsx
@@ -19,7 +19,7 @@ const templates = [
   { id: 'academic', name: 'Academic', imageUrl: 'https://i.imghippo.com/files/jrJS7434DBo.png', aiHint: 'academic cv' },
   { id: 'corporate', name: 'Corporate', imageUrl: 'https://i.imghippo.com/files/mM6306YPQ.png', aiHint: 'corporate resume' },
   { id: 'elegant', name: 'Elegant', imageUrl: 'https://i.imghippo.com/files/S0gWf1722429953.png', aiHint: 'elegant resume' },
-  { id: 'bold', name: 'Bold', imageUrl: 'https://placehold.co/300x400.png?text=Bold', aiHint: 'bold resume' },
+  { id: 'bold', name: 'Bold', imageUrl: 'https://i.imghippo.com/files/MwX4896mA.png', aiHint: 'bold resume' },
   { id: 'modern', name: 'Modern', imageUrl: 'https://placehold.co/300x400.png?text=Modern', aiHint: 'modern resume' },
 ] as const;
 
