40b224718fde8739d000b79390a92cda613e724f
velumbalaji <velumbalaji@gmail.com>
2025-11-07 18:23:05 +0000

    Add Next.js configuration file

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        remotePatterns: [
+            { protocol: 'https', hostname: 'cdn.builder.io' },
+            { protocol: 'https', hostname: 'i.imghippo.com' },
+        ],
+    },
 };
 
 export default nextConfig;
