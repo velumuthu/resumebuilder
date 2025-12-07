6db11463a31e6a1562c69a72c422e5b812ea7958
velumbalaji <velumbalaji@gmail.com>
2025-12-07 13:19:08 +0000

    New

--- src/app/impact/page.tsx
+++ src/app/impact/page.tsx
@@ -11,7 +11,7 @@ export default function ImpactPage() {
       <main className="container mx-auto py-12 px-4">
         <div className="prose prose-invert max-w-4xl mx-auto bg-background p-8 rounded-lg shadow-lg">
             <h1>Our Impact</h1>
-            <script async="async" data-cfasync="false" src="//hardypistol.com/0b2761533619c7ae9692d519a7e5bcbf/invoke.js"></script>
+            <script async data-cfasync={false} src="//hardypistol.com/0b2761533619c7ae9692d519a7e5bcbf/invoke.js"></script>
             <div id="container-0b2761533619c7ae9692d519a7e5bcbf"></div>
         </div>
       </main>
